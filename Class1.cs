9ab4fa8b697e15f8ba1d3fbd037048bd5c9955d5
Shreya tiwari <shreya.tiwari@zensar.com>
2020-07-31 01:04:13 +0530

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_30Jul
+{
+    class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+
+
+        public void Add()
+        {
+            Console.WriteLine("Add method with no parameter...");
+        }
+
+
+
+        public void Add(string str, int x)
+        {
+            Console.WriteLine(str + x);
+        }
+
+
+
+        public void Add(int a, string str)
+        {
+            Console.WriteLine(str + a);
+        }
+
+
+
+    }
+    class MethodOverloading
+    {
+
+
+
+        static void Main()
+        {
+            Calculator calc = new Calculator();
+            calc.Add();
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Quadrilateral.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Assignment_30Jul
+{
+    abstract class Shape
+    {
+        public int length;
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(int _length)
+        {
+            length = _length;
+        }
+
+        public abstract int Area();
+
+
+    }
+
+    class Square : Shape
+    {
+        public Square() { }
+
+        public Square(int _length)
+        {
+
+        }
+        public override int Area()
+
+        {
+            return length*length ; 
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public int breadth;
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int _length, int _breadth)
+        {
+            breadth = _breadth;
+            length = _length;
+            
+        }
+        public override int Area()
+        {
+            return length * breadth;
+        }
+    }
+
+
+
+
+
+    class Quadrilateral
+
+    {
+        static void Main(string[]args)
+        {
+            Square s = new Square(4);
+            s.Area();
+
+
+
+
+
+            Rectangle r = new Rectangle(5, 6);
+            r.Area();
+
+
+
+            if (s.Area() > r.Area())
+            {
+                Console.WriteLine("square area is greater than rectangle area");
+            }
+            else
+            {
+                Console.WriteLine("rectangle area is greater than square");
+            }
+
+
+
+
+            Console.Read();
+
+
+        }
+    }
+}
